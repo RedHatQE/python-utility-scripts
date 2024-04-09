e0f9041761f03bc00ab6ed8462a932d39ee762da
dbasunag <dbasunag@redhat.com>
2024-04-09 09:04:53 -0400

    remove extra test

--- tests/polarion/test_polarion_automated.py
+++ tests/polarion/test_polarion_automated.py
@@ -15,16 +15,3 @@ def test_missing_project_id_set_automated():
     )
     assert "Polarion project id must be passed via config file or command line" in err
     assert not rc
-
-
-def test_project_id_from_config_set_automated():
-    command = f"{BASE_COMMAND} --config-file-path=config.example.yaml"
-    rc, _, err = run_command(
-        command=shlex.split(command),
-        verify_stderr=False,
-        check=False,
-        capture_output=False,
-        stderr=subprocess.PIPE,
-    )
-    assert not err
-    assert rc
