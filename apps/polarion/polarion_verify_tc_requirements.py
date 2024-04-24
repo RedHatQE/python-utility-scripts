676b64b6a2bf8d149d1c25f355435d8a6d59b2b7
Debarati Basu-Nag <dbasunag@redhat.com>
2024-04-24 08:38:13 -0400

    Revert "base PR for set polarion as automated (#30)" (#34)
    This reverts commit 5245a918a94a173abc57d7806a8609cdbed5a481.

--- apps/polarion/polarion_verify_tc_requirements.py
+++ apps/polarion/polarion_verify_tc_requirements.py
@@ -1,8 +1,13 @@
+import re
 import logging
 from simple_logger.logger import get_logger
 import os
 import click
-from apps.polarion.polarion_utils import validate_polarion_requirements, find_polarion_ids, get_polarion_project_id
+from apps.polarion.polarion_utils import (
+    git_diff_lines,
+    validate_polarion_requirements,
+)
+from apps.utils import get_util_config
 
 LOGGER = get_logger(name="polarion-verify-tc-requirements")
 
@@ -19,16 +24,27 @@ LOGGER = get_logger(name="polarion-verify-tc-requirements")
 def has_verify(config_file_path, project_id, verbosity):
     if verbosity:
         LOGGER.setLevel(logging.DEBUG)
-    polarion_project_id = get_polarion_project_id(
-        project_id=project_id, config_file_path=config_file_path, util_name="pyutils-polarion-verify-tc-requirements"
-    )
-    if added_ids := find_polarion_ids(polarion_project_id=polarion_project_id, string_to_match="added"):
+
+    polarion_project_id = project_id or get_util_config(
+        util_name="pyutils-polarion-verify-tc-requirements",
+        config_file_path=config_file_path,
+    ).get("project_id")
+
+    if not polarion_project_id:
+        LOGGER.error("Polarion project id must be passed via config file or command line")
+        raise click.Abort()
+
+    if added_ids := re.findall(
+        rf"pytest.mark.polarion.*({polarion_project_id}-[0-9]+)",
+        "\n".join(git_diff_lines().get("added", [])),
+        re.MULTILINE | re.IGNORECASE,
+    ):
         LOGGER.debug(f"Checking following ids: {added_ids}")
         if tests_with_missing_requirements := validate_polarion_requirements(
             polarion_test_ids=added_ids,
             polarion_project_id=polarion_project_id,
         ):
-            LOGGER.error(f"TestCases with missing requirement: {tests_with_missing_requirements}")
+            click.echo(f"TestCases with missing requirement: {tests_with_missing_requirements}")
             raise click.Abort()
 
 
