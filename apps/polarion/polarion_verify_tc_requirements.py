21624a99fe51ce997da75f040ca9d3dd01e3fbda
Debarati Basu-Nag <dbasunag@redhat.com>
2024-05-23 08:48:12 -0400

    base PR for set polarion as automated (#36)
    * Revert "Revert "base PR for set polarion as automated (#30)" (#34)"
    
    This reverts commit 676b64b6a2bf8d149d1c25f355435d8a6d59b2b7.
    
    * Update apps/polarion/polarion_set_automated.py
    
    Co-authored-by: Ruth Netser <rnetser@redhat.com>
    
    * updates to review comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * use typing
    
    * coverage report percentage change
    
    * updates based on review comments
    
    ---------
    
    Co-authored-by: Ruth Netser <rnetser@redhat.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- apps/polarion/polarion_verify_tc_requirements.py
+++ apps/polarion/polarion_verify_tc_requirements.py
@@ -1,13 +1,10 @@
-import re
 import logging
 from simple_logger.logger import get_logger
 import os
 import click
-from apps.polarion.polarion_utils import (
-    git_diff_lines,
-    validate_polarion_requirements,
-)
-from apps.utils import get_util_config
+
+from apps.polarion.exceptions import PolarionTestCaseWithoutRequirementError
+from apps.polarion.polarion_utils import validate_polarion_requirements, find_polarion_ids, get_polarion_project_id
 
 LOGGER = get_logger(name="polarion-verify-tc-requirements")
 
@@ -20,32 +17,24 @@ LOGGER = get_logger(name="polarion-verify-tc-requirements")
     default=os.path.expanduser("~/.config/python-utility-scripts/config.yaml"),
 )
 @click.option("--project-id", "-p", help="Provide the polarion project id")
-@click.option("--verbosity", default=False, is_flag=True)
-def has_verify(config_file_path: str, project_id: str, verbosity: bool) -> None:
-    if verbosity:
-        LOGGER.setLevel(logging.DEBUG)
-
-    polarion_project_id = project_id or get_util_config(
-        util_name="pyutils-polarion-verify-tc-requirements",
-        config_file_path=config_file_path,
-    ).get("project_id")
-
-    if not polarion_project_id:
-        LOGGER.error("Polarion project id must be passed via config file or command line")
-        raise click.Abort()
-
-    if added_ids := re.findall(
-        rf"pytest.mark.polarion.*({polarion_project_id}-[0-9]+)",
-        "\n".join(git_diff_lines().get("added", [])),
-        re.MULTILINE | re.IGNORECASE,
-    ):
-        LOGGER.debug(f"Checking following ids: {added_ids}")
+@click.option("--verbose", default=False, is_flag=True)
+def has_verify(config_file_path: str, project_id: str, verbose: bool) -> None:
+    if verbose:
+        LOGGER.setLevel(logging.INFO)
+    else:
+        logging.disable(logging.ERROR)
+    polarion_project_id = project_id or get_polarion_project_id(
+        config_file_path=config_file_path, util_name="pyutils-polarion-verify-tc-requirements"
+    )
+    if added_ids := find_polarion_ids(polarion_project_id=polarion_project_id, string_to_match="added"):
+        LOGGER.info(f"Checking following ids: {added_ids}")
         if tests_with_missing_requirements := validate_polarion_requirements(
             polarion_test_ids=added_ids,
             polarion_project_id=polarion_project_id,
         ):
-            click.echo(f"TestCases with missing requirement: {tests_with_missing_requirements}")
-            raise click.Abort()
+            raise PolarionTestCaseWithoutRequirementError(
+                f"TestCases with missing requirement: {tests_with_missing_requirements}"
+            )
 
 
 if __name__ == "__main__":
---
+++ tests/polarion/test_polarion_automated.py
@@ -0,0 +1,17 @@
+import shlex
+import subprocess
+from pyhelper_utils.shell import run_command
+
+BASE_COMMAND = "poetry run python apps/polarion/polarion_set_automated.py --verbose"
+
+
+def test_missing_project_id_set_automated():
+    rc, _, err = run_command(
+        command=shlex.split(BASE_COMMAND),
+        verify_stderr=False,
+        check=False,
+        capture_output=False,
+        stderr=subprocess.PIPE,
+    )
+    assert "Polarion project id must be passed via config file or command line" in err
+    assert not rc
