b373c656b87258bbeb933fe569077afc3e1607af
dbasunag <dbasunag@redhat.com>
2024-08-02 19:09:16 -0400

    updates to jira

--- apps/jira_utils/get_jira_information.py
+++ apps/jira_utils/get_jira_information.py
@@ -1,11 +1,18 @@
+import logging
 import os
+import re
+
 import click
+from jira import JIRAError
 
 from simple_logger.logger import get_logger
 
-from apps.jira_utils.utils import get_jiras_from_python_files, get_closed_jira_ids, get_jira_connection_params, \
-    get_jira_version_mismatch, JiraConnector
-from apps.utils import ListParamType
+from apps.jira_utils.exceptions import JiraValidationError, JiraInvalidConfigFileError
+from apps.jira_utils.jira_utils import get_jiras_from_python_files, JiraConnector
+from apps.utils import ListParamType, get_util_config
+from typing import Any, Dict
+
+DEFAULT_RESOLVED_STATUS = ["verified", "release pending", "closed", "resolved"]
 
 LOGGER = get_logger(name=__name__)
 
@@ -19,22 +26,69 @@ LOGGER = get_logger(name=__name__)
 )
 @click.option(
     "--jira-target-versions",
-    help="Provide comma separated list of Jira target version for the bugs.",
+    help="Provide comma separated list of Jira target version, for version validation against a repo branch.",
     type=ListParamType(),
-    required=True
+    required=False,
 )
-def get_closed_jiras(jira_cfg_file, jira_target_versions):
-    jira_connector = JiraConnector(cfg_file=jira_cfg_file)
-    jira_id_dict = get_jiras_from_python_files()
-    if jira_errors:= get_closed_jira_ids(jira_connector=jira_connector,
-                                        jira_ids_dict=jira_id_dict):
-        click.echo(f"Following jiras are not open or could not be accessed: {jira_errors}")
-        raise click.Abort()
-    if version_mismatch := get_jira_version_mismatch(jira_connector=jira_connector, jira_id_dict=jira_id_dict,
-                                                     jira_expected_versions=jira_target_versions):
-        click.echo(f"Following jiras are not matching expected version{jira_target_versions}: {version_mismatch}")
-        raise click.Abort()
+@click.option("--verbose", default=False, is_flag=True)
+def get_jira_mismatch(jira_cfg_file: Any, jira_target_versions: Any, verbose: bool) -> Any:
+    if verbose:
+        LOGGER.setLevel(logging.DEBUG)
+    else:
+        logging.disable(logging.CRITICAL)
+    config_dict = get_util_config(util_name="pyutils-jira", config_file_path=jira_cfg_file)
+    if not (config_dict.get("url") and config_dict.get("token")):
+        raise JiraInvalidConfigFileError("Jira config file must contain valid url and token.")
+    jira_connector = JiraConnector(token=config_dict["token"], url=config_dict["url"])
+    jira_error: Dict[str, Any] = {"status_mismatch": {}, "version_mismatch": {}, "connection_error": {}}
+    resolved_status = config_dict.get("resolved_statuses") or DEFAULT_RESOLVED_STATUS
+    jira_target_versions = jira_target_versions or config_dict.get("jira_target_versions", [])
+    skip_project_ids = config_dict.get("skip_project_ids", [])
+    for file_name in (jira_id_dict := get_jiras_from_python_files()):
+        for jira_id in jira_id_dict[file_name]:
+            try:
+                # check closed status:
+                jira_issue_metadata = jira_connector.get_issue(jira_id=jira_id).fields
+                current_jira_status = jira_issue_metadata.status.name.lower()
+                if current_jira_status in resolved_status:
+                    jira_error["status_mismatch"].setdefault(file_name, []).append(
+                        f"{jira_id}: current status: {current_jira_status}"
+                    )
+                # validate correct target version if provided:
+                if jira_target_versions:
+                    if skip_project_ids and jira_id.startswith(tuple(skip_project_ids)):
+                        continue
+                    fix_version = (
+                        re.search(r"([\d.]+)", jira_issue_metadata.fixVersions[0].name)
+                        if (jira_issue_metadata.fixVersions)
+                        else None
+                    )
+                    current_target_version = fix_version.group(1) if fix_version else "vfuture"
+                    if not any([current_target_version == version for version in jira_target_versions]):
+                        jira_error["version_mismatch"].setdefault(file_name, []).append(
+                            f"{jira_id}: target version: {current_target_version}]"
+                        )
+
+            except JIRAError as exp:
+                jira_error["connection_error"].setdefault(file_name, []).append(
+                    f"{jira_id}: status code: {exp.status_code}, details: {exp.text}]."
+                )
+    jira_error = {key: value for key, value in jira_error.items() if value}
+    if jira_error.values():
+        error = "Following Jira ids failed jira check:\n"
+        if jira_error.get("status_mismatch"):
+            error += f" Jira ids in resolved state: {jira_error['status_mismatch']}."
+        if jira_error.get("version_mismatch"):
+            error += (
+                f" Jira expected versions: {jira_target_versions}, "
+                f"current versions: {jira_error['version_mismatch']}."
+            )
+        if jira_error.get("connection_error"):
+            error += f" Jira ids with connection error: {jira_error['connection_error']}."
+        LOGGER.error(error)
+        raise JiraValidationError(error)
+    LOGGER.info("Successfully completed Jira validations")
 
 
 if __name__ == "__main__":
-    get_closed_jiras()
+    get_jira_mismatch()
--- tests/jira_utils/test_jira.py
+++ tests/jira_utils/test_jira.py
@@ -1,34 +1,21 @@
-from apps.jira_utils.get_jira_information import get_closed_jiras
-from tests.utils import get_cli_runner
+import shlex
+import subprocess
+
+from pyhelper_utils.shell import run_command
+
 from simple_logger.logger import get_logger
-from unittest import mock
 
 LOGGER = get_logger(name=__name__)
+BASE_COMMAND = "poetry run python apps/jira_utils/get_jira_information.py --verbose"
 
 
-def test_jira_missing_target_version():
-    result = get_cli_runner().invoke(get_closed_jiras, "--jira-cfg-file example.jira.cfg")
-    LOGGER.info(f"Result output: {result.output}, exit code: {result.exit_code}, exceptions: {result.exception}")
-    assert result.exit_code != 0
-    assert "Missing option '--jira-target-versions'" in result.output
-
-
-def test_jira_missing_cfg_file():
-    result = get_cli_runner().invoke(get_closed_jiras, '--jira-cfg-file jira.cfg --jira-target-versions "ABC"')
-    LOGGER.info(f"Result output: {result.output}, exit code: {result.exit_code}, exceptions: {result.exception}")
-    assert result.exit_code == 1
-    assert "Jira config file is required." in result.output
-
-
-def test_jira_with_closed_bugs():
-    with mock.patch("apps.jira_utils.get_jira_information.JiraConnector") as jira_connector:
-        a_instance = jira_connector.return_value
-        a_instance.get_issue_metadata.return_value = {'status':{'name':'closed'}}
-        with mock.patch("apps.jira_utils.get_jira_information.get_jiras_from_python_files") as jiras:
-            jiras.return_value = {"file1": ['ABC-1234']}
-            with mock.patch("apps.jira_utils.get_jira_information.JiraConnector.get_closed_jira_ids") as closed_ids:
-                closed_ids.return_value = "{'file1': ['ABC-1234' ['closed']]}"
-                result = get_cli_runner().invoke(get_closed_jiras, '--jira-target-versions "ABC"')
-                assert result.exit_code == 1
-                #assert f"Following jiras are not open or could not be accessed:" in result.output
-            LOGGER.info(f"Result output: {result.output}, exit code: {result.exit_code}, exceptions: {result.exception}")
+def test_jira_missing_config_file():
+    rc, _, err = run_command(
+        command=shlex.split(f"{BASE_COMMAND} --jira-cfg-file invalid-jira.cfg"),
+        verify_stderr=False,
+        check=False,
+        capture_output=False,
+        stderr=subprocess.PIPE,
+    )
+    assert "Jira config file must contain valid url and token" in err
+    assert not rc
