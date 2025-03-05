2ae43a3db5a0144ad92c7279ea8b5b4597bebec6
Meni Yakove <myakove@gmail.com>
2025-03-05 17:37:43 +0200

    Use ast to check if function are not used

--- apps/unused_code/unused_code.py
+++ apps/unused_code/unused_code.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import ast
 import logging
 import os
-import subprocess
 import sys
+from collections import deque
 from concurrent.futures import Future, ThreadPoolExecutor, as_completed
 from typing import Any, Iterable
 
@@ -14,6 +14,43 @@ from simple_logger.logger import get_logger
 from apps.utils import ListParamType, all_python_files, get_util_config
 
 LOGGER = get_logger(name=__name__)
+
+
+class FuncCallVisitor(ast.NodeVisitor):
+    def __init__(self) -> None:
+        self._name: Any = deque()
+
+    @property
+    def name(self) -> str:
+        return self._name[-1]
+
+    @name.deleter
+    def name(self) -> None:
+        self._name.clear()
+
+    def visit_Name(self, node: Any) -> None:
+        self._name.appendleft(node.id)
+
+    def visit_Attribute(self, node: Any) -> None:
+        try:
+            self._name.appendleft(node.attr)
+            self._name.appendleft(node.value.id)
+        except AttributeError:
+            self.generic_visit(node)
+
+
+def get_func_calls(py_file: str) -> list[str]:
+    func_calls = []
+    with open(py_file) as fd:
+        tree = ast.parse(fd.read())
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Call):
+            callvisitor = FuncCallVisitor()
+            callvisitor.visit(node.func)
+            func_calls.append(callvisitor.name)
+
+    return func_calls
 
 
 def is_fixture_autouse(func: ast.FunctionDef) -> bool:
@@ -52,7 +89,9 @@ def is_ignore_function_list(ignore_prefix_list: list[str], function: ast.Functio
     return False
 
 
-def process_file(py_file: str, func_ignore_prefix: list[str], file_ignore_list: list[str]) -> str:
+def process_file(
+    py_file: str, func_ignore_prefix: list[str], file_ignore_list: list[str], functions_calls: list[str]
+) -> str:
     if os.path.basename(py_file) in file_ignore_list:
         LOGGER.debug(f"Skipping file: {py_file}")
         return ""
@@ -69,23 +108,7 @@ def process_file(py_file: str, func_ignore_prefix: list[str], file_ignore_list: 
             LOGGER.debug(f"Skipping `autouse` fixture function: {func.name}")
             continue
 
-        used = False
-        _func_grep_found = subprocess.check_output(["git", "grep", "-w", func.name], shell=False)
-
-        for entry in _func_grep_found.decode().splitlines():
-            _, _line = entry.split(":", 1)
-
-            if f"def {func.name}" in _line:
-                continue
-
-            if _line.strip().startswith("#"):
-                continue
-
-            if func.name in _line:
-                used = True
-                break
-
-        if not used:
+        if func.name not in functions_calls:
             return f"{os.path.relpath(py_file)}:{func.name}:{func.lineno}:{func.col_offset} Is not used anywhere in the code."
 
     return ""
@@ -119,23 +142,36 @@ def get_unused_functions(
     func_ignore_prefix = exclude_function_prefixes or unused_code_config.get("exclude_function_prefix", [])
     file_ignore_list = exclude_files or unused_code_config.get("exclude_files", [])
 
-    jobs: list[Future] = []
-    if not os.path.exists(".git"):
-        LOGGER.error("Must be run from a git repository")
-        sys.exit(1)
+    _functions_calls_jobs: list[Future] = []
+    _unused_functions_jobs: list[Future] = []
+
+    _functions_calls: list[str] = []
 
     with ThreadPoolExecutor() as executor:
         for py_file in all_python_files():
-            jobs.append(
+            _functions_calls_jobs.append(
+                executor.submit(
+                    get_func_calls,
+                    py_file=py_file,
+                )
+            )
+
+        for result in as_completed(_functions_calls_jobs):
+            if _calls := result.result():
+                _functions_calls.extend(_calls)
+
+        for py_file in all_python_files():
+            _unused_functions_jobs.append(
                 executor.submit(
                     process_file,
                     py_file=py_file,
                     func_ignore_prefix=func_ignore_prefix,
                     file_ignore_list=file_ignore_list,
+                    functions_calls=_functions_calls,
                 )
             )
 
-        for result in as_completed(jobs):
+        for result in as_completed(_unused_functions_jobs):
             if unused_func := result.result():
                 unused_functions.append(unused_func)
 
